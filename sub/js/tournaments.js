a7cf30a0781a8cfa0e926cf86c58117c62d315d3
Sky Williamson <96271755+SlawSimulation@users.noreply.github.com>
2025-06-19 00:35:19 +0100

    feat. Updated Tournaments Script with catching errors

--- sub/js/tournaments.js
+++ sub/js/tournaments.js
@@ -1,8 +1,17 @@
 fetch('sub/data/tournaments.json')
-  .then(res => res.json())
-  .then(data => {
-    // Display tournaments from data
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`Failed to fetch tournaments: ${res.status} ${res.statusText}`);
+    }
+    return res.json();
   })
-  .catch(() => {
-    // Handle errors
+  .then(data => {
+    console.log('Tournaments fetched:', data);
+
+    // TODO: Display tournaments from data
+    // Example: renderTournaments(data);
+  })
+  .catch(error => {
+    console.error('Error loading tournaments:', error);
+    // TODO: Show user-friendly error message in UI
   });
